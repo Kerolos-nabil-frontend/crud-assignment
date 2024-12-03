f182b0dc611be3950ed19d4aa27540f67a645f53
Kerolos-nabil-frontend <Keronabil53@yahoo.com>
2024-12-04 01:51:55 +0200

    second commit

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var bookmarkLink = document.getElementById('bookmarkLink')
 var addBtn = document.getElementById('addBtn')
 var linkList= [];
 
-if(localStorage.getItem('links') == null) {
+if(localStorage.getItem('links')) {
 linkList= JSON.parse(localStorage.getItem('links')) ;
-}else{
-    linkList= [];
 }
 
 function addLink(){
@@ -24,19 +22,22 @@ function addLink(){
     linkList.push(link)
     localStorage.setItem('links',JSON.stringify(linkList))
     displayLink()
-    console.log(linkList)
+    
 }
-
+addBtn.addEventListener('click',function(){
+    addLink()
+    clearForm()
+})
 // display \\
 
 function displayLink(){
-    var cartoona=``
+    var cartoona=''
     for (var i=0;i<linkList.length;i++){
         cartoona+= ` <tr >
                         <td>${i+1}</td>
                         <td>${linkList[i].bookmarkName}</td>
-                        <td><button class="btn btn-info "><i class="fa-regular fa-eye"></i> Visit</button></td>
-                        <td><button class="btn btn-danger" onclick="clearForm()"><i class="fa-solid fa-trash"></i> Delete</button></td>
+                        <td><button class="btn btn-info " onclick="visititem(${i})"><i class="fa-regular fa-eye"></i> Visit</button></td>
+                        <td><button class="btn btn-danger" onclick="deletelink(${i})"><i class="fa-solid fa-trash"></i> Delete</button></td>
                     </tr>`
     }
     document.getElementById('content').innerHTML = cartoona
@@ -44,6 +45,19 @@ function displayLink(){
 }
 
 function clearForm(){
-    document.getElementById('content').innerHTML =''
+    bookmarkName.value= null
+    bookmarkLink.value= null
    
+}
+
+
+function deletelink(i){
+    linkList.splice(i,1)
+    localStorage.setItem('links',JSON.stringify(linkList))
+    displayLink()
+}
+
+
+function visititem(i){
+    window.open(linkList[i].bookmarkLink)
 }
